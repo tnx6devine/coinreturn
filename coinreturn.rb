af0efc769c0f2dc94c0799d6c334fadf9cf04651
tnx6devine <tnx6devine@gmail.com>
2017-12-02 15:23:04 -0500

    accounted for nickel, penny, and 0

--- coinreturn.rb
+++ coinreturn.rb
@@ -1,7 +1,13 @@
 def change_maker_3000(change)
 	coin = {}
-	if change > 0
+	if change > 0 && change < 5
 		coin[:penny] = change
+	elsif change > 4
+		coin[:nickel] = change / 5
+		change = change % 5
+		if change > 0
+			coin[:penny] = change
+		end
 	end
 	coin
 end
