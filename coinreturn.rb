a0b028fdbd110b4080beabc864eea45b6e787b5d
tnx6devine <tnx6devine@gmail.com>
2017-12-03 13:18:52 -0500

    condensed code

--- coinreturn.rb
+++ coinreturn.rb
@@ -1,23 +1,36 @@
 def change_maker_3000(change)
-	coin = {}
-	if change > 0 && change < 5
-		coin[:penny] = change
-	elsif change > 4 && change < 10
-		coin[:nickel] = change / 5
-		change = change % 5
-		if change > 0
-			coin[:penny] = change
-		end
-	elsif change > 9
-		coin[:dime] = change / 10
-		change = change % 10
-		if change > 4
-			coin[:nickel] = change / 5
-			change = change % 5
-			if change > 0
-				coin[:penny] = change
-			end
+	coins_returned = {}
+	coin_values = coins = {:dollar => 100, :quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
+	coin_values.each do |money, value|
+		if change >= value
+			coins_returned[money] = change / value
+			change = change % value
 		end
 	end
-	coin
+	coins_returned
 end
+
+
+
+# 	if change > 0 && change < 5
+# 		coin[:penny] = change
+# 	elsif change > 4 && change < 10
+# 		coin[:nickel] = change / 5
+# 		change = change % 5
+# 		if change > 0
+# 			coin[:penny] = change
+# 		end
+# 	elsif change > 9
+# 		coin[:dime] = change / 10
+# 		change = change % 10
+# 		if change > 4
+# 			coin[:nickel] = change / 5
+# 			change = change % 5
+# 			if change > 0
+# 				coin[:penny] = change
+# 			end
+# 		end
+# 	end
+# 	coin
+# end
+
